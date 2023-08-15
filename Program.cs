0a62b9225b1c658d24637f1c3470764cab7a9b47
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-15 16:32:47 +0530

    Suman : Created a Contacts in Address Book with first and last names, addresses, city, state, zip, phone number and email

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using AddressBookRevisit;
+using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book Program!");
+
+            List<Contact> contacts = new List<Contact>();
+
+            // Create a new contact
+            Contact newContact = new Contact
+            {
+                FirstName = "Suman",
+                LastName = "jha",
+                Address = "123-xyz",
+                City = "bhliwara",
+                State = "Rjasthan",
+                Zip = "311001",
+                PhoneNumber = "0123456789",
+                Email = "suman@email.com",
+            };
+
+            // Add the new contact to the list
+            contacts.Add(newContact);
+
+            // Display the added contact
+            Console.WriteLine("\nContact added:");
+            Console.WriteLine($"Name: {newContact.FirstName} {newContact.LastName}");
+            Console.WriteLine($"Address: {newContact.Address}");
+            Console.WriteLine($"City: {newContact.City}");
+            Console.WriteLine($"State: {newContact.State}");
+            Console.WriteLine($"Zip: {newContact.Zip}");
+            Console.WriteLine($"Phone Number: {newContact.PhoneNumber}");
+            Console.WriteLine($"Email: {newContact.Email}");
+
             Console.ReadLine();
         }
     }
