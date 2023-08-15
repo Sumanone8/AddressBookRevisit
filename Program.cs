98698b62f8a6ea6d72b628f7c47d566d8447f880
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-15 16:53:08 +0530

    Suman : Updated code for addition of new contact in the address book.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using MyAddressBook;
 using System;
-using System.Collections.Generic;
 
 namespace MyAddressBook
 {
@@ -10,33 +9,25 @@ namespace MyAddressBook
         {
             Console.WriteLine("Welcome to Address Book Program!");
 
-            List<Contact> contacts = new List<Contact>();
+            AddressBook addressBook = new AddressBook();
 
-            // Create a new contact
+            // Create and add a new contact
             Contact newContact = new Contact
             {
-                FirstName = "Suman",
-                LastName = "jha",
-                Address = "123-xyz",
-                City = "bhliwara",
-                State = "Rjasthan",
-                Zip = "311001",
-                PhoneNumber = "0123456789",
-                Email = "suman@email.com",
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Main St",
+                City = "Sample City",
+                State = "Sample State",
+                Zip = "12345",
+                PhoneNumber = "555-1234",
+                Email = "john@example.com"
             };
 
-            // Add the new contact to the list
-            contacts.Add(newContact);
+            addressBook.AddContact(newContact);
 
             // Display the added contact
-            Console.WriteLine("\nContact added:");
-            Console.WriteLine($"Name: {newContact.FirstName} {newContact.LastName}");
-            Console.WriteLine($"Address: {newContact.Address}");
-            Console.WriteLine($"City: {newContact.City}");
-            Console.WriteLine($"State: {newContact.State}");
-            Console.WriteLine($"Zip: {newContact.Zip}");
-            Console.WriteLine($"Phone Number: {newContact.PhoneNumber}");
-            Console.WriteLine($"Email: {newContact.Email}");
+            addressBook.DisplayContacts();
 
             Console.ReadLine();
         }
