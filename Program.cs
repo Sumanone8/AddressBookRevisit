976d7d1eac7966941c6aa61c0cd855654595ac87
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-15 17:08:00 +0530

    Suman : Updated code to add new contact with console

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using MyAddressBook;
 using System;
+using System.Collections.Generic;
 
 namespace MyAddressBook
 {
@@ -11,25 +12,61 @@ namespace MyAddressBook
 
             AddressBook addressBook = new AddressBook();
 
-            // Create and add a new contact
-            Contact newContact = new Contact
+            // Read contact details from the user
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            // Create and add a new contact if input is valid
+            if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName) &&
+                !string.IsNullOrEmpty(address) && !string.IsNullOrEmpty(city) &&
+                !string.IsNullOrEmpty(state) && !string.IsNullOrEmpty(zip) &&
+                !string.IsNullOrEmpty(phoneNumber) && !string.IsNullOrEmpty(email))
             {
-                FirstName = "John",
-                LastName = "Doe",
-                Address = "123 Main St",
-                City = "Sample City",
-                State = "Sample State",
-                Zip = "12345",
-                PhoneNumber = "555-1234",
-                Email = "john@example.com"
-            };
+                Contact newContact = new Contact
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Address = address,
+                    City = city,
+                    State = state,
+                    Zip = zip,
+                    PhoneNumber = phoneNumber,
+                    Email = email
+                };
 
-            addressBook.AddContact(newContact);
+                addressBook.AddContact(newContact);
 
-            // Display the added contact
-            addressBook.DisplayContacts();
+                // Display the added contact
+                addressBook.DisplayContacts();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Contact not added.");
+            }
 
             Console.ReadLine();
         }
     }
 }
+
